a985e3a2f057eab66542f44e4d59aba14ade6c12
Oli <olivia.konishif@gmail.com>
2026-03-19 15:23:40 -0300

    ejercicio1

---
+++ p1ej1.py
@@ -0,0 +1,5 @@
+anio = int(input("ingrese año de nacimiento: "))
+
+print ("cumplis 18 en el año: ", anio + 18)
+print("Cumplís 21 en el año: ", anio + 21)
+print("Cumplís 100 en el año: ", anio + 100)
