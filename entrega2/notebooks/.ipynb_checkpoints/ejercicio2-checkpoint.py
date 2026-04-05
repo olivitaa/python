a2b766dbf87d2ebd87a3404404d3f4f94aed3e14
Oli <olivia.konishif@gmail.com>
2026-04-05 20:56:25 -0300

    prueba competencia cocina

---
+++ entrega2/notebooks/.ipynb_checkpoints/ejercicio2-checkpoint.py
@@ -0,0 +1,14 @@
+playlist = [
+{"title": "Bohemian Rhapsody", "duration": "5:55"},
+{"title": "Hotel California", "duration": "6:30"},
+{"title": "Stairway to Heaven", "duration": "8:02"},
+{"title": "Imagine", "duration": "3:07"},
+{"title": "Smells Like Teen Spirit", "duration": "5:01"},
+{"title": "Billie Jean", "duration": "4:54"},
+{"title": "Hey Jude", "duration": "7:11"},
+{"title": "Like a Rolling Stone", "duration": "6:13"},
+]
+
+totalsegundos = 0
+cancion_larga 
+cancion_corta = 
